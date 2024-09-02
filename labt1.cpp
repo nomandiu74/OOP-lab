d29f6a96d304a3ca7703827006aa22d754517d52
nomandiu74 <148541616+nomandiu74@users.noreply.github.com>
2024-09-02 08:50:56 +0600

    Add files via upload

---
+++ labt1.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+class AccountHolder
+
+{
+private:
+    int id;
+    string name;
+    string gmail;
+    double balance;
+    string mobileNumber;
+
+public:
+    void set_details(int ID,string Name,string Gmail,double Balance,string mobileNUmber)
+    {
+        id=ID;
+        name=Name;
+        gmail=Gmail;
+        balance=Balance;
+        mobileNumber=mobileNUmber;
+    }
+
+    void display()
+    {
+        cout<<"ID:"<<id<<endl;
+        cout<<"NAME:"<<name<<endl;
+        cout<<"Gmail:"<<gmail<<endl;
+        cout<<"Balance:"<<balance<<endl;
+        cout<<"Mobile Number:"<<mobileNumber<<endl;
+        cout << "----------------------------------" << endl;
+        cout<<endl;
+
+    }
+};
+
+int main()
+{
+AccountHolder noman;
+noman.set_details(001,"Abdulla AL Noman","nomandiu9@gmail.com",50000.00,"01521770989");
+noman.display();
+
+
+AccountHolder mahmud;
+mahmud.set_details(002,"Mahmudul","mahmuddiu9@gmail.com",10000.00,"0152390753");
+mahmud.display();
+
+
+AccountHolder ayman;
+ayman.set_details(003,"ayman","ayman@gmail.com",50000.00,"01521779823489");
+ayman.display();
+
+
+return 0;
+
+
+}
